0a3ea4fe0517365007cc7da0ac1df3e002d2dd21
SMITH MOGAMAT SHUHAIR <126153069+kxngzero329@users.noreply.github.com>
2023-07-06 13:32:12 +0200

    Update tally-footer.js

--- DWA 14/components/tally-footer.js
+++ DWA 14/components/tally-footer.js
@@ -8,7 +8,7 @@ class TallyFooter extends LitElement {
   
   render() {
     return html`
-      <footer style ="text-align:center; margin:auto; padding-top:50px; font-size:25px">
+      <footer style ="text-align:center; margin:auto; padding-top:50px; font-size:25px; color:white">
         Inspired by
         <a class="footer__link" href="https://tallycount.app/">Tally Count</a>.
       </footer>
